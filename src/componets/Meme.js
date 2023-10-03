426d56aaec79c0296d46df45100a4c0972139f6f
Chaitanya Kale <kale.chaitanya0304@gmail.com>
2023-10-03 10:56:30 +0530

    create meme componet

---
+++ src/componets/Meme.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <form>
+            <input/>
+            <input/>
+            <button>Get a new meme image</button>
+        </form>
+    )
+}
